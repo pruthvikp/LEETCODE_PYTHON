c25c41856c2d29dadb2c4936fa4612185f2af09a
Pruthvi K P <117765336+pruthvikp@users.noreply.github.com>
2024-01-11 07:08:57 +0530

    Update 9_PalindromeNumber.py

--- 9_PalindromeNumber.py
+++ 9_PalindromeNumber.py
@@ -15,6 +15,11 @@ Output: false
 Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 '''
 
+
+# Approach: 
+# Convert the integer x to a string and use string slicing ([::-1]) to reverse the string. 
+# Check if the original string is equal to its reverse and return True if it's a palindrome, False otherwise.
+
 class Solution(object):
     def isPalindrome(self, x):
         """
