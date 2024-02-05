be3fce57b194307831f4f5fd10007b295108b2b3
Pruthvi K P <117765336+pruthvikp@users.noreply.github.com>
2024-02-05 20:52:51 +0530

    Update 141_LinkedListCycle.py

--- Linked List/141_LinkedListCycle.py
+++ Linked List/141_LinkedListCycle.py
@@ -41,18 +41,20 @@ Follow up: Can you solve it using O(1) (i.e. constant) memory?
 #         self.val = x
 #         self.next = None
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution(object):
     def hasCycle(self, head):
         """
         :type head: ListNode
         :rtype: bool
         """
-        if head is None:
-            return False
-        if head.next is None:
-            return False
         slow,fast=head,head
-        while fast is not None and fast.next is not None:
+        while fast and fast.next:
             slow=slow.next
             fast=fast.next.next
             if slow==fast:
